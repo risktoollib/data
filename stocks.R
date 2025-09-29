8e5ae5ff5ed25c27407e102a5fa238877e1d0755
Philippe Cote <pcote@ualberta.ca>
2025-09-29 07:50:58 -0600

    Update stocks.R

--- stocks.R
+++ stocks.R
@@ -1,6 +1,10 @@
+install.packages(c("feather","rvest","readxl")
 library(tidyverse)
 library(tidyquant)
-install.packages("feather")
+library(rvest)
+library(readxl)
+                 
+# STOCKS
 tick <- c("XLE", "XOM","QQQ","SPY","GLD","TLT","COW","EEM")
 prices <- tick %>%
   tidyquant::tq_get(get = "stock.prices", from = "2005-01-01") %>%
@@ -16,8 +20,6 @@ stocks <- list(prices = prices %>%
                  tidyr::drop_na())
 
 jsonlite::write_json(stocks, "stocks.json", pretty = TRUE)
-
-###
 
 sp400_desc <- tidyquant::tq_index("SP400") #%>% dplyr::filter(!stringr::str_detect(symbol,"BRK.B|BF.B"))
 
@@ -41,3 +43,38 @@ sp400_joined <- sp400_prices %>%
 feather::write_feather(sp400_desc,"sp400_desc.feather")
 feather::write_feather(sp400_prices,"sp400_prices.feather")
 feather::write_feather(sp400_joined,"sp400_joined.feather")
+
+# POWER
+url = "https://www.ercot.com/misapp/GetReports.do?reportTypeId=13060&reportTitle=Historical%20DAM%20Load%20Zone%20and%20Hub%20Prices&showHTMLView=&mimicKey"
+urls <- rvest::read_html(url) %>%
+  rvest::html_elements("body > form > table") %>%
+  rvest::html_elements("a") %>%
+  rvest::html_attr("href") %>%
+  paste0("https://www.ercot.com",.)
+
+for (i in 1:length(urls)) {
+
+  url <- urls[i]
+  destfile <- paste0("ercot.zip")
+  curl::curl_download(url, destfile)
+  utils::unzip(destfile)
+  ff <- list.files(pattern = "rpt",)
+  if (i == 1) {
+    ercot <- lapply(readxl::excel_sheets(ff), read_excel, path = ff) %>% do.call(rbind, .)
+  } else {
+    tmp <- lapply(readxl::excel_sheets(ff), read_excel, path = ff) %>% do.call(rbind, .)
+    ercot <- rbind(ercot, tmp)
+  }
+  file.remove(ff)
+  file.remove(destfile)
+}
+
+ercot <- ercot %>%
+  dplyr::as_tibble(.name_repair = "universal") %>%
+  dplyr::mutate(Delivery.Date = as.Date(Delivery.Date, "%m/%d/%Y"),
+                Hour.Ending = lubridate::hm(Hour.Ending),
+                Date = lubridate::as_datetime(paste(Delivery.Date, Hour.Ending))) %>%
+  dplyr::select(Date, everything())
+feather::write_feather(ercot,"ercot.feather")
+
+                 
